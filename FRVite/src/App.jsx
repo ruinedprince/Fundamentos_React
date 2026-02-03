b6f3c4a8027789cb28227f88336db9eef1370f5a
ruinedprince <jsttmaciel89@gmail.com>
2026-02-03 15:01:27 -0300

    Aplicado estilização ao textarea renderizado na página principal, ao App.jsx e a página principal do projeto

--- FRVite/src/App.jsx
+++ FRVite/src/App.jsx
@@ -1,12 +1,16 @@
 import TextInput from './components/TextInput'
+import styles from './App.module.css'
+
 
 function App() {
   return (
     <div>
-      <TextInput placeholder={"O que está acontecendo?"} maxLength={125}/> {/* Componentes devem ser sempre iniciados com letra maiúscula pois são componentes React, se não, será considerado um elemento HTML. Quando criamos componentes, podemos criar quantos quisermos, basta chamá-los dentro do componente pai. Atributos devem ser passados como props, que são objetos que contêm as propriedades do componente. */}
-      <TextInput placeholder={"Me diga uma novidade"} maxLength={250}/>
-      <TextInput placeholder={"Me diga uma novidade"} maxLength={250} value={"Qualquer coisa"}/> {/* Mesmo que eu não tenha definido o valor da propriedade value, posso passar um valor fixo direto no componente.*/}
-      <TextInput maxLength={250}/> {/* Mesmo que eu não tenha definido o valor da propriedade placeholder, posso passar um valor padrão para o componente isso se chama fallback value ou valor padrão.*/}
+
+      {/* Componentes devem ser sempre iniciados com letra maiúscula pois são componentes React, se não, será considerado um elemento HTML. Quando criamos componentes, podemos criar quantos quisermos, basta chamá-los dentro do componente pai. Atributos devem ser passados como props, que são objetos que contêm as propriedades do componente. */}
+      {/* Mesmo que eu não tenha definido o valor da propriedade value, posso passar um valor fixo direto no componente.*/}
+      {/* Mesmo que eu não tenha definido o valor da propriedade placeholder, posso passar um valor padrão para o componente isso se chama fallback value ou valor padrão.*/}
+
+      <TextInput className={styles.appContainer} placeholder={"O que está acontecendo?"} maxLength={125}/>
     </div>
   )
 }
--- FRVite/src/components/TextInput.jsx
+++ FRVite/src/components/TextInput.jsx
@@ -1,9 +1,19 @@
+import styles from './TextInput.module.css';
+
 // Props são objetos que contêm as propriedades do componente. São passados como argumentos para o componente. Podemos acessar as propriedades do componente através do objeto props.
 
-export default function TextInput({placeholder = "Valor padrão", maxLength, ...props}) { // Como eu ja sei que vou receber as propriedades placeholder e maxLength, posso desestruturar o objeto props e receber as propriedades que eu quero. Se não receber, o valor padrão será "Valor padrão".
+export default function TextInput({placeholder = "Valor padrão", maxLength, className, ...props}) {
+
+    // Como eu ja sei que vou receber as propriedades placeholder e maxLength, posso desestruturar o objeto props e receber as propriedades que eu quero. Se não receber, o valor padrão será "Valor padrão".
+
     return (
-        <div>
-            <textarea placeholder={placeholder} maxLength={maxLength} {...props}/> {/* O código entre as chaves é o valor da propriedade placeholder. O problema é que valores fixos direto no componente não são recomendados, pois se precisarmos alterar o valor, teremos que alterar o código fonte, por isso, usamos uma variável para armazenar o valor da propriedade.*/}
+        <div className={className}>
+
+            {/* O código entre as chaves é o valor da propriedade placeholder. O problema é que valores fixos direto no componente não são recomendados, pois se precisarmos alterar o valor, teremos que alterar o código fonte, por isso, usamos uma variável para armazenar o valor da propriedade.
+            */}
+            {/* Como eu quero que o textarea tenha a classe input, posso usar o className={styles.input} para aplicar a classe input. Eu não posso usar apena "class" pois isso é uma palavra reservada do JavaScript.*/}
+
+            <textarea className={styles.input} placeholder={placeholder} maxLength={maxLength} {...props}/> 
         </div>
     )
 }
