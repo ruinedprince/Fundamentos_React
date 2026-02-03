4b9f755d300cfb05b8eac9cfe4794737297f291b
ruinedprince <jsttmaciel89@gmail.com>
2026-02-03 16:29:45 -0300

    Introduzi o uso de funções com mudança de estados conforme o valor definido dinamicamente

--- FRVite/src/components/TextInput.jsx
+++ FRVite/src/components/TextInput.jsx
@@ -6,14 +6,18 @@ export default function TextInput({placeholder = "Valor padrão", maxLength, cla
 
     // Como eu ja sei que vou receber as propriedades placeholder e maxLength, posso desestruturar o objeto props e receber as propriedades que eu quero. Se não receber, o valor padrão será "Valor padrão".
 
+    const [contador, setContador] = useState(0);
+    
+
     return (
-        <div className={className}>
+        <div className={`${styles.container} ${className || ''}`}>
 
             {/* O código entre as chaves é o valor da propriedade placeholder. O problema é que valores fixos direto no componente não são recomendados, pois se precisarmos alterar o valor, teremos que alterar o código fonte, por isso, usamos uma variável para armazenar o valor da propriedade.
             */}
             {/* Como eu quero que o textarea tenha a classe input, posso usar o className={styles.input} para aplicar a classe input. Eu não posso usar apena "class" pois isso é uma palavra reservada do JavaScript.*/}
 
-            <textarea className={styles.input} placeholder={placeholder} maxLength={maxLength} {...props}/> 
+            <textarea className={styles.input} placeholder={placeholder} maxLength={maxLength} {...props}/>
+            <p>{contador}/{limiteContador}</p>
         </div>
     )
 }
