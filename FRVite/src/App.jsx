48c5a9fbd820de89adbf6bc11870c0dacab11e14
ruinedprince <jsttmaciel89@gmail.com>
2026-02-03 14:18:10 -0300

    Adicionado o componente de TextInput com a tag textarea para ser renderizado na página principal

--- FRVite/src/App.jsx
+++ FRVite/src/App.jsx
@@ -1,6 +1,10 @@
+import TextInput from './components/TextInput'
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <TextInput placeholder="Digite algo..."/> {/* Componentes devem ser sempre iniciados com letra maiúscula pois são componentes React, se não, será considerado um elemento HTML. Quando criamos componentes, podemos criar quantos quisermos, basta chamá-los dentro do componente pai.  */}
+    </div>
   )
 }
 
---
+++ FRVite/src/components/TextInput.jsx
@@ -0,0 +1,7 @@
+export default function TextInput() {
+    return (
+        <div>
+            <textarea />
+        </div>
+    )
+}
