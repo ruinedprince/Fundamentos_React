8030d61f7cfed83e9b60362f2875780c726877a8
ruinedprince <jsttmaciel89@gmail.com>
2026-02-03 14:31:24 -0300

    Implementado diversos testes com props em componentes, como passagem de valor de váriavel de componentes, fallback de props e renderização com diferentes valores

--- FRVite/src/App.jsx
+++ FRVite/src/App.jsx
@@ -3,7 +3,10 @@ import TextInput from './components/TextInput'
 function App() {
   return (
     <div>
-      <TextInput placeholder="Digite algo..."/> {/* Componentes devem ser sempre iniciados com letra maiúscula pois são componentes React, se não, será considerado um elemento HTML. Quando criamos componentes, podemos criar quantos quisermos, basta chamá-los dentro do componente pai.  */}
+      <TextInput placeholder={"O que está acontecendo?"} maxLength={125}/> {/* Componentes devem ser sempre iniciados com letra maiúscula pois são componentes React, se não, será considerado um elemento HTML. Quando criamos componentes, podemos criar quantos quisermos, basta chamá-los dentro do componente pai. Atributos devem ser passados como props, que são objetos que contêm as propriedades do componente. */}
+      <TextInput placeholder={"Me diga uma novidade"} maxLength={250}/>
+      <TextInput placeholder={"Me diga uma novidade"} maxLength={250} value={"Qualquer coisa"}/> {/* Mesmo que eu não tenha definido o valor da propriedade value, posso passar um valor fixo direto no componente.*/}
+      <TextInput maxLength={250}/> {/* Mesmo que eu não tenha definido o valor da propriedade placeholder, posso passar um valor padrão para o componente isso se chama fallback value ou valor padrão.*/}
     </div>
   )
 }
--- FRVite/src/components/TextInput.jsx
+++ FRVite/src/components/TextInput.jsx
@@ -1,7 +1,9 @@
-export default function TextInput() {
+// Props são objetos que contêm as propriedades do componente. São passados como argumentos para o componente. Podemos acessar as propriedades do componente através do objeto props.
+
+export default function TextInput({placeholder = "Valor padrão", maxLength, ...props}) { // Como eu ja sei que vou receber as propriedades placeholder e maxLength, posso desestruturar o objeto props e receber as propriedades que eu quero. Se não receber, o valor padrão será "Valor padrão".
     return (
         <div>
-            <textarea />
+            <textarea placeholder={placeholder} maxLength={maxLength} {...props}/> {/* O código entre as chaves é o valor da propriedade placeholder. O problema é que valores fixos direto no componente não são recomendados, pois se precisarmos alterar o valor, teremos que alterar o código fonte, por isso, usamos uma variável para armazenar o valor da propriedade.*/}
         </div>
     )
 }
