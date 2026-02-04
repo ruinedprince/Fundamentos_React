5d9dc4bebf74928de89a752e1f2843267c5456e7
ruinedprince <jsttmaciel89@gmail.com>
2026-02-04 13:44:01 -0300

    Testes com componentes controlados e não controlados utilizando e exibindo o defaultValue do textArea

--- FRVite/src/components/TextInput.jsx
+++ FRVite/src/components/TextInput.jsx
@@ -1,5 +1,5 @@
 import styles from './TextInput.module.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 // Props são objetos que contêm as propriedades do componente. São passados como argumentos para o componente. Podemos acessar as propriedades do componente através do objeto props.
 
@@ -8,12 +8,17 @@ export default function TextInput({placeholder = "Valor padrão", maxLength, cla
     // Como eu ja sei que vou receber as propriedades placeholder e maxLength, posso desestruturar o objeto props e receber as propriedades que eu quero. Se não receber, o valor padrão será "Valor padrão".
 
     const [text, setText] = useState('');
-    
+    const inputElement = useRef();
+
     function onTextChange(event) {
         const text = event.target.value;
         if (text.length <= maxLength) {
             setText(text);
         }
+    }
+
+    if (inputElement.current) { // Verifica se o elemento existe antes de acessar sua propriedade defaultValue.
+        console.log(inputElement.current.defaultValue);
     }
 
     return (
@@ -24,11 +29,12 @@ export default function TextInput({placeholder = "Valor padrão", maxLength, cla
             {/* Como eu quero que o textarea tenha a classe input, posso usar o className={styles.input} para aplicar a classe input. Eu não posso usar apena "class" pois isso é uma palavra reservada do JavaScript.*/}
 
             <textarea
+                ref={inputElement}
                 className={styles.input}
                 placeholder={placeholder}
                 onChange={onTextChange}
                 maxLength={maxLength}
-                value={text}
+                defaultValue={10} // Valor padrão do textarea. O contador de caracteres não será atualizado pois o valor é fixo. Para contornar isso, usamos o useRef para referenciar o textarea e atualizar o valor do textarea.
                 {...props}
             />
             <p>{text.length}/{maxLength}</p>
